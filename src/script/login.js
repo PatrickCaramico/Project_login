c35563b557a6482281800b9e0215a6cbf6a529a3
Patrick Caramico <patrickcaramico88@gmail.com>
2024-05-17 09:56:12 -0300

    Update login.js
    Atualização do script.

--- src/script/login.js
+++ src/script/login.js
@@ -1,16 +1,20 @@
-function logar () {
-    var user = document.getElementById("username")
-    var senha = document.getElementById("senha")
+function logar() {
+    var user = document.getElementById("username");
+    var senha = document.getElementById("senha");
 
-    if (user.value == "admin@admin.com" && senha.value == "123") {
+    if (user.value == "admin" && senha.value == "123") {
         localStorage.setItem("acesso", true);
 
-        alert("Credenciais Validadas com sucesso! Seja Bem Vindo Usuario!");
+        alert("Credenciais Validadas! Seja Bem Vindo Patrick Caramico!");
 
         window.location.href = "tela_contagem examplo/sistema.html";
     } else {
-        alert("Usuário ou senha incorretos!")
-        user.value = ""
-        senha.value = ""
+        alert("Usuário ou senha incorretos!");
+        user.value = "";
+        senha.value = "";
     }
 }
+
+window.onload = function() {
+    alert("Utilize as seguintes credenciais para ter acesso:\nUSERNAME: admin / SENHA: 123");
+};
